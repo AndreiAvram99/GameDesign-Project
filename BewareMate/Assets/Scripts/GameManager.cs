85d2f63407746d1926524f5643d9bf9ee44c803e
Rusu <rusudaniel5799@gmail.com>
2021-04-04 15:33:09 +0300

    tried to add score text (almost done)

--- BewareMate/Assets/Scripts/GameManager.cs
+++ BewareMate/Assets/Scripts/GameManager.cs
@@ -1,10 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public float moveSpeed = 1.0f;
     public Vector3 moveVector;
     public GameObject floor;
+
+    public static Text textScore;
+    public static int score = 0;
+
+    public void Start()
+    {
+        // textScore = GameObject.Find("Score").GetComponent<Text>();
+        StartCoroutine(time());
+    }
+    IEnumerator time()
+    {
+        while (true)
+        {
+            timeCount();
+            // DE SETAT score la acel text "Score: [score]"
+            // Debug.Log(textScore);
+            yield return new WaitForSeconds(1);
+        }
+    }
+    void timeCount()
+    {
+        score += 1;
+    }
 }
